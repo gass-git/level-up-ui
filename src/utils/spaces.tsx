1b4f4764d8b426be82cfeb0adb8648a01547e482
Gabriel Salinas Szada <75760163+gass-git@users.noreply.github.com>
2026-04-08 12:02:25 +0200

    chore: removes unused icons

--- src/utils/spaces.tsx
+++ src/utils/spaces.tsx
@@ -1,11 +1,5 @@
 import type { JSX } from "react";
 
-const space1: JSX.Element = <span>&nbsp;</span>;
-const space2: JSX.Element = <span>&nbsp;&nbsp;</span>;
-const space3: JSX.Element = <span>&nbsp;&nbsp;&nbsp;</span>;
-
 export default function space(n: number): JSX.Element {
   return <span>{"\u00A0".repeat(n)}</span>;
 }
-
-export { space1, space2, space3 };
